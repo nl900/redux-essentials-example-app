dd29bf5580ccc64620a41fb66b662146c600807c
Nancy Lee <nancy.lee@alexsolutions.com.au>
2020-11-15 10:58:55 +1100

    define prepare callback function

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,9 +9,20 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
+    postAdded: {
+      reducer(state, action) {
         state.push(action.payload)
       },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content
+          }
+        }
+      }
+    },
       postUpdated(state, action) {
         const { id, title, content } = action.payload
         const existingPost = state.find(post => post.id === id)
